fc28f10a076e4b9d53d81a4cc33882b38956e7a4
Markusloka <Markuslokander97@gmail.com>
2023-01-12 15:56:06 +0100

    Jesus

--- visiba-park/src/components/myCalendar.tsx
+++ visiba-park/src/components/myCalendar.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
+import "../App.css";
 
 export function Mycalendar() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div>
+    <div className="styleCalendar">
       <Calendar onChange={onChange} value={value} />
     </div>
   );
