c81a96bbc3b9ca8b06dcb8f7e73d7bad1f0fccfa
Markusloka <Markuslokander97@gmail.com>
2023-01-20 13:38:41 +0100

    Some css fixes and added a route from sign in.

--- visiba-park/src/pages/Sign_in.tsx
+++ visiba-park/src/pages/Sign_in.tsx
@@ -1,10 +1,14 @@
 import "../App.css";
 import slackLogo from "../assets/slack-new-logo.svg";
+import { useNavigate } from "react-router-dom";
+
 function Signin() {
+  const navigate = useNavigate();
+
   return (
     <div className="signIn">
-      <button className="signIn-button">
-        <p className="loginbutton">Logga in med Slack</p>
+      <button className="signIn-button" onClick={() => navigate("/")}>
+        <p className="loginbutton">Log in with Slack</p>
         <img src={slackLogo} className="slacklogo" alt="slack logo" />
       </button>
     </div>
