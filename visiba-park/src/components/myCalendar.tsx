07e3062c839a6e5489d5a4e4be001666d7afed54
Markusloka <Markuslokander97@gmail.com>
2023-01-31 13:59:58 +0100

    Added min and max date with help from max

--- visiba-park/src/components/myCalendar.tsx
+++ visiba-park/src/components/myCalendar.tsx
@@ -5,10 +5,17 @@ import "../App.css";
 
 export function Mycalendar() {
   const [date, setDate] = useState(new Date());
+  const today = new Date();
+  const maxDate = new Date(today.setMonth(today.getMonth() + 1));
 
   return (
     <div className="styleCalendar">
-      <Calendar onChange={setDate} value={date} />
+      <Calendar
+        onChange={setDate}
+        value={date}
+        maxDate={maxDate}
+        minDate={new Date()}
+      />
       <p className="text-center">
         <span className="bold">Selected Date:</span> {date.toDateString()}
       </p>
