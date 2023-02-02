5e7b400e264b538a4f6df71602c314eded72d9ad
Markusloka <Markuslokander97@gmail.com>
2023-02-02 17:59:27 +0100

    updated supabase and fixed an issue with supabase.

--- visiba-park/src/database/supabase.js
+++ visiba-park/src/database/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://vheyvgfbvknlijdnvfrn.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoZXl2Z2ZidmtubGlqZG52ZnJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzMyODA3MTgsImV4cCI6MTk4ODg1NjcxOH0.ruv0RSTdUtUFiIbXHunC6bgmNW7FCH2j6JmQhIw1urA";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function signInWithSlack() {
