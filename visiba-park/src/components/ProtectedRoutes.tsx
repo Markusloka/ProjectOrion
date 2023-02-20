142da3eb7c84c5844cb85e5e98a1ec77c42c26bb
Markusloka <Markuslokander97@gmail.com>
2023-02-20 13:02:36 +0100

    Pox fixed it

--- visiba-park/src/components/ProtectedRoutes.tsx
+++ visiba-park/src/components/ProtectedRoutes.tsx
@@ -2,8 +2,8 @@ import { Navigate, Outlet } from "react-router-dom";
 import supabase from "../database/supabase";
 
 const PrivateRoutes = () => {
-  const auth = supabase.auth.getUser;
-  return auth ? <Outlet /> : <Navigate to="/Signin" />;
+  const auth = supabase.auth.getUser();
+  return auth ? <Navigate to="/Signin" /> : <Outlet />;
 };
 
 export default PrivateRoutes;
