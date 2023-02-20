40ab4485897f4561a98e9a275c5fe42e7497ed4b
Markusloka <Markuslokander97@gmail.com>
2023-02-20 14:08:18 +0100

    small changes

--- visiba-park/src/pages/Sign_in.tsx
+++ visiba-park/src/pages/Sign_in.tsx
@@ -2,11 +2,13 @@ import "../App.css";
 import slackLogo from "../assets/slack-new-logo.svg";
 import { signInWithSlack } from "../database/supabase.js";
 import { useNavigate } from "react-router-dom";
+import auth from "../components/ProtectedRoutes";
 
 function Signin() {
   const navigate = useNavigate();
   function handleSignInButtonClick() {
     signInWithSlack();
+    auth();
   }
 
   return (
