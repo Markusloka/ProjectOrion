67fc9d399c97cb37a8eedcd2a19c8c31ea166853
Markusloka <Markuslokander97@gmail.com>
2023-02-28 15:33:45 +0100

    Please work

--- visiba-park/src/App.tsx
+++ visiba-park/src/App.tsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import PrivateRoutes from "./components/ProtectedRoutes";
 import Home from "./pages/Home";
 import Signin from "./pages/Sign_in";
 
 function App() {
   return (
     <Routes>
-      <Route element={<PrivateRoutes />}>
-        <Route element={<Home />} path="/" />
-      </Route>
+      <Route element={<Home />} path="/" />
+
       <Route element={<Signin />} path="/Signin" />
     </Routes>
   );
