fdcaf2cfe3bea7eb89a37b695294b47e9feb7488
Markusloka <Markuslokander97@gmail.com>
2023-02-28 16:49:57 +0100

    The tree helped me with the check.

--- visiba-park/src/components/myCalendar.tsx
+++ visiba-park/src/components/myCalendar.tsx
@@ -19,13 +19,13 @@ export default function Mycalendar() {
   }
 
   async function createBooking() {
+    if ((await isBooked(date)) === null) return;
     await supabase
       .from("bookning")
       .insert([{ datum: selectDate(), Namn: "xd" }]);
 
     setDate(date);
     fetchBookings();
-    isBooked();
   }
 
   async function isBooked(date: Date): Promise<null | string> {
