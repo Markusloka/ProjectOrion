6daba3f7475ef8b8936fa8b8614aeaffde4d411b
Markusloka <Markuslokander97@gmail.com>
2023-03-03 15:39:13 +0100

    button switching does not work yet plz help

--- visiba-park/src/pages/Home.tsx
+++ visiba-park/src/pages/Home.tsx
@@ -1,14 +1,44 @@
 import Mycalendar from "../components/myCalendar";
 import visibaLogo from "../assets/visiba_logo.svg";
 import "../App.css";
+import supabase from "../database/supabase";
+
+async function LogoutBtn() {
+  const { error } = await supabase.auth.signOut();
+  return (
+    <div className="authentication">
+      <button className="signInButton" /*onClick={error}*/>
+        Logout{" error"}
+      </button>
+    </div>
+  );
+}
+
+function LoginBtn() {
+  return (
+    <a href="/Signin">
+      <button className="signInButton">Login</button>
+    </a>
+  );
+}
+
+function AuthBtn() {
+  const isLoggedIn = supabase.auth.getUser();
+  if (isLoggedIn) {
+    console.log(isLoggedIn);
+    return <LogoutBtn />;
+  }
+  console.log(isLoggedIn);
+  return <LoginBtn />;
+}
 
 function Home() {
   return (
     <div className="Home">
-      <a className="singnInHome" href="/Signin">
+      {/* <a className="singnInHome" href="/Signin">
         <button className=" signInButton">Logga in</button>
-      </a>
-
+      </a> */}
+      <AuthBtn />
       <div className="logoTitle">
         <img src={visibaLogo} className="logo react" alt="React logo" />
       </div>
