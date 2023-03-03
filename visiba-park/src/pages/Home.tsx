ab18755743859be9283e715417391d3ad99df7e9
Markusloka <Markuslokander97@gmail.com>
2023-03-03 15:47:19 +0100

    Max plz help

--- visiba-park/src/pages/Home.tsx
+++ visiba-park/src/pages/Home.tsx
@@ -3,34 +3,34 @@ import visibaLogo from "../assets/visiba_logo.svg";
 import "../App.css";
 import supabase from "../database/supabase";
 
-async function LogoutBtn() {
-  const { error } = await supabase.auth.signOut();
-  return (
-    <div className="authentication">
-      <button className="signInButton" /*onClick={error}*/>
-        Logout{" error"}
-      </button>
-    </div>
-  );
-}
+// async function LogoutBtn() {
+//   const { error } = await supabase.auth.signOut();
+//   return (
+//     <div className="authentication">
+//       <button className="signInButton" onClick={error}>
+//         Logout{" error"}
+//       </button>
+//     </div>
+//   );
+// }
 
-function LoginBtn() {
-  return (
-    <a href="/Signin">
-      <button className="signInButton">Login</button>
-    </a>
-  );
-}
+// function LoginBtn() {
+//   return (
+//     <a href="/Signin">
+//       <button className="signInButton">Login</button>
+//     </a>
+//   );
+// }
 
-function AuthBtn() {
-  const isLoggedIn = supabase.auth.getUser();
-  if (isLoggedIn) {
-    console.log(isLoggedIn);
-    return <LogoutBtn />;
-  }
-  console.log(isLoggedIn);
-  return <LoginBtn />;
-}
+// function AuthBtn() {
+//   const isLoggedIn = supabase.auth.getUser();
+//   if (isLoggedIn) {
+//     console.log(isLoggedIn);
+//     return <LogoutBtn />;
+//   }
+//   console.log(isLoggedIn);
+//   return <LoginBtn />;
+// }
 
 function Home() {
   return (
@@ -38,7 +38,7 @@ function Home() {
       {/* <a className="singnInHome" href="/Signin">
         <button className=" signInButton">Logga in</button>
       </a> */}
-      <AuthBtn />
+      {/* <AuthBtn /> */}
       <div className="logoTitle">
         <img src={visibaLogo} className="logo react" alt="React logo" />
       </div>
