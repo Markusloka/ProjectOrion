a53f4379f9411b468270d661e046ebbe12e4c1f2
Markusloka <Markuslokander97@gmail.com>
2023-03-14 13:00:44 +0100

    added Sign in button for Azure, but does not work yet

--- visiba-park/src/pages/Sign_in.tsx
+++ visiba-park/src/pages/Sign_in.tsx
@@ -1,4 +1,5 @@
 import "../App.css";
+import Azure from "../assets/Azure.svg";
 import slackLogo from "../assets/slack-new-logo.svg";
 import supabase from "../database/supabase";
 
@@ -14,11 +15,29 @@ const SignIn = () => {
     return { error, data };
   }
 
+  async function signInButton2() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "azure",
+      options: {
+        scopes: "email",
+      },
+    });
+    if (error) {
+      console.log(error);
+    }
+
+    return { error, data };
+  }
+
   return (
     <div className="signIn">
       <button className="signIn-button" onClick={signInButton}>
         <p className="loginbutton">Log in with Slack</p>
         <img src={slackLogo} className="slacklogo" alt="slack logo" />
+      </button>
+      <button className="signIn-button2" onClick={signInButton2}>
+        <p className="loginbutton">Log in with Azure</p>
+        <img src={Azure} className="slacklogo" alt="slack logo" />
       </button>
     </div>
   );
