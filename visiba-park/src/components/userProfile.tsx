4750d2f83c119f580c798ca9d32492707288bb23
Markusloka <Markuslokander97@gmail.com>
2023-03-19 00:55:04 +0100

    hmmm works on my machine but not when hosted.

--- visiba-park/src/components/userProfile.tsx
+++ visiba-park/src/components/userProfile.tsx
@@ -1,44 +1,43 @@
 import "../App.css";
 import { User } from "@supabase/supabase-js";
-import { userInfo } from "os";
 
 interface Props {
   user: User | null;
 }
-
 function UserProfile({ user }: Props) {
-  const loggedInUserProfile = user;
-  if (loggedInUserProfile) {
-    const { user_metadata } = loggedInUserProfile;
-    if (user_metadata) {
-      const { avatar_url } = user_metadata;
-
-      if (avatar_url) {
-        const userProfileImage = document.getElementById("user-profile-image");
-        if (userProfileImage) {
-          userProfileImage.setAttribute("src", avatar_url);
-          userProfileImage.setAttribute("alt", "User Profile Image");
-        }
-      }
-    }
-  }
-
-  const loggedInUser = user;
-  if (loggedInUser) {
-    const { user_metadata } = loggedInUser;
-    if (user_metadata) {
-      const { full_name } = user_metadata;
-
-      if (full_name) {
-        const userFullNameDiv = document.getElementById("user-fullname");
-        if (userFullNameDiv) {
-          userFullNameDiv.textContent = full_name;
-        }
-      }
-    }
-  }
-
   if (user) {
+    const loggedInUserProfile = user;
+    if (loggedInUserProfile) {
+      const { user_metadata } = loggedInUserProfile;
+      if (user_metadata) {
+        const { avatar_url } = user_metadata;
+
+        if (avatar_url) {
+          const userProfileImage =
+            document.getElementById("user-profile-image");
+          if (userProfileImage) {
+            userProfileImage.setAttribute("src", avatar_url);
+            userProfileImage.setAttribute("alt", "User Profile Image");
+          }
+        }
+      }
+    }
+
+    const loggedInUser = user;
+    if (loggedInUser) {
+      const { user_metadata } = loggedInUser;
+      if (user_metadata) {
+        const { full_name } = user_metadata;
+
+        if (full_name) {
+          const userFullNameDiv = document.getElementById("user-fullname");
+          if (userFullNameDiv) {
+            userFullNameDiv.textContent = full_name;
+          }
+        }
+      }
+    }
+
     return (
       <div className="userElements">
         <img
