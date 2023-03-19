30420d2a2f8870fae9beb8dc6021c543f0465503
Markusloka <Markuslokander97@gmail.com>
2023-03-19 12:07:22 +0100

    perhaps this will work.

--- visiba-park/src/components/userProfile.tsx
+++ visiba-park/src/components/userProfile.tsx
@@ -5,39 +5,37 @@ interface Props {
   user: User | null;
 }
 function UserProfile({ user }: Props) {
+  const loggedInUserProfile = user;
+  if (loggedInUserProfile) {
+    const { user_metadata } = loggedInUserProfile;
+    if (user_metadata) {
+      const { avatar_url } = user_metadata;
+
+      if (avatar_url) {
+        const userProfileImage = document.getElementById("user-profile-image");
+        if (userProfileImage) {
+          userProfileImage.setAttribute("src", avatar_url);
+          userProfileImage.setAttribute("alt", "User Profile Image");
+        }
+      }
+    }
+  }
+
+  const loggedInUser = user;
+  if (loggedInUser) {
+    const { user_metadata } = loggedInUser;
+    if (user_metadata) {
+      const { full_name } = user_metadata;
+
+      if (full_name) {
+        const userFullNameDiv = document.getElementById("user-fullname");
+        if (userFullNameDiv) {
+          userFullNameDiv.textContent = full_name;
+        }
+      }
+    }
+  }
   if (user) {
-    const loggedInUserProfile = user;
-    if (loggedInUserProfile) {
-      const { user_metadata } = loggedInUserProfile;
-      if (user_metadata) {
-        const { avatar_url } = user_metadata;
-
-        if (avatar_url) {
-          const userProfileImage =
-            document.getElementById("user-profile-image");
-          if (userProfileImage) {
-            userProfileImage.setAttribute("src", avatar_url);
-            userProfileImage.setAttribute("alt", "User Profile Image");
-          }
-        }
-      }
-    }
-
-    const loggedInUser = user;
-    if (loggedInUser) {
-      const { user_metadata } = loggedInUser;
-      if (user_metadata) {
-        const { full_name } = user_metadata;
-
-        if (full_name) {
-          const userFullNameDiv = document.getElementById("user-fullname");
-          if (userFullNameDiv) {
-            userFullNameDiv.textContent = full_name;
-          }
-        }
-      }
-    }
-
     return (
       <div className="userElements">
         <img
@@ -48,6 +46,8 @@ function UserProfile({ user }: Props) {
         <div id="user-fullname" className="user"></div>
       </div>
     );
+  } else {
+    return null;
   }
 }
 
