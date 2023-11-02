2c40ac26fed4b2c17482d95d09c5661055ed9d05
Markusloka <Markuslokander97@gmail.com>
2023-11-02 17:26:54 +0100

    Now work

--- visiba-park/src/App.tsx
+++ visiba-park/src/App.tsx
@@ -2,13 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import { useUser } from "./components/UserUser";
 import Home from "./pages/Home";
 import Signin from "./pages/Sign_in";
+import { User } from "@supabase/supabase-js";
 
 function App() {
   const [user, logout] = useUser();
 
   return (
     <Routes>
-      <Route element={<Home user={user} logout={logout} />} path="/" />
+      <Route
+        element={<Home user={user} logout={logout} date={new Date()} namn="" />}
+        path="/"
+      />
 
       <Route element={<Signin />} path="/Signin" />
     </Routes>
